2b07b3f7a6e14efea0a4b97165c3dce0b35276e9
Nguyen Khac Hung <hungngk98@gmail.com>
2023-04-12 09:50:14 +0700

    Format code; Add javascript to options in settings;

---
+++ pages/setting/script.js
@@ -0,0 +1,51 @@
+const restoreSettings = async () => {
+    const defaultSettings = await chrome.storage.local.get([
+        'real-time-protection',
+        'machine-learning-detecting',
+        'auto-report',
+        'enable-shopee-title',
+        'enable-href',
+    ]);
+
+    document.querySelector('[value="real-time-protection"]').checked = defaultSettings['real-time-protection'];
+    document.querySelector('[value="machine-learning-detecting"]').checked =
+        defaultSettings['machine-learning-detecting'];
+    document.querySelector('[value="auto-report"]').checked = defaultSettings['auto-report'];
+    document.querySelector('[value="enable-shopee-title"]').checked = defaultSettings['enable-shopee-title'];
+    document.querySelector('[value="enable-href"]').checked = defaultSettings['enable-href'];
+};
+
+const saveSettings = async () => {
+    const realtimeProtection = document.querySelector('[value="real-time-protection"]').checked;
+    const machineLearningDetecting = document.querySelector('[value="machine-learning-detecting"]').checked;
+    const autoReport = document.querySelector('[value="auto-report"]').checked;
+    const enableShopeeTitle = document.querySelector('[value="enable-shopee-title"]').checked;
+    const enableHref = document.querySelector('[value="enable-href"]').checked;
+
+    await chrome.storage.local.set({
+        'real-time-protection': realtimeProtection,
+        'machine-learning-detecting': machineLearningDetecting,
+        'auto-report': autoReport,
+        'enable-shopee-title': enableShopeeTitle,
+        'enable-href': enableHref,
+    });
+
+    document.getElementById('btn-discard').style.display = 'none';
+
+    await chrome.runtime.sendMessage('update-context-menus');
+
+    alert('Saved successfully');
+};
+
+const discardChanges = () => {
+    window.location.reload();
+};
+
+document.addEventListener('DOMContentLoaded', restoreSettings);
+document.getElementById('btn-save').addEventListener('click', saveSettings);
+document.getElementById('btn-discard').addEventListener('click', discardChanges);
+document.querySelectorAll('.all-settings input').forEach((inputTag) => {
+    inputTag.addEventListener('change', () => {
+        document.getElementById('btn-discard').style.display = 'initial';
+    });
+});
--- service_worker.js
+++ service_worker.js
@@ -4,36 +4,74 @@ async function getCurrentTab() {
     return tab;
 }
 
-chrome.runtime.onInstalled.addListener(() => {
+async function updateContextMenus() {
+    const settings = await chrome.storage.local.get();
+    if (!settings['real-time-protection']) {
+        chrome.contextMenus.remove('real-time-protection');
+    } else {
+        chrome.contextMenus.create({
+            id: 'real-time-protection',
+            title: 'Real-time Protection',
+        });
+    }
+}
+
+chrome.runtime.onInstalled.addListener(async () => {
+    const defaultSettings = {
+        'real-time-protection': true,
+        'machine-learning-detecting': true,
+        'auto-report': false,
+        'enable-shopee-title': true,
+        'enable-href': false,
+    };
+    await chrome.storage.local.set(defaultSettings);
+
+    if (defaultSettings['real-time-protection']) {
+        chrome.contextMenus.create({
+            id: 'real-time-protection',
+            title: 'Real-time Protection',
+        });
+    }
+
     chrome.contextMenus.create({
-        id: "reveal",
-        title: "View detail link",
-        contexts: ['link']
+        id: 'reveal',
+        title: 'View detail link',
+        contexts: ['link'],
     });
-    
+
     chrome.contextMenus.create({
-        id: "report",
-        title: "Report link",
-        contexts: ['link']
+        id: 'report',
+        title: 'Report link',
+        contexts: ['link'],
     });
+});
+
+chrome.runtime.onMessage.addListener(async (mess) => {
+    if (mess === 'update-context-menus') {
+        await updateContextMenus();
+    }
 });
 
 chrome.contextMenus.onClicked.addListener(async (info) => {
     if (info.menuItemId === 'reveal') {
-        chrome.scripting.executeScript({
+        await chrome.scripting.executeScript({
             target: { tabId: (await getCurrentTab()).id },
-            func: (info) => { alert(`The actual link is: ${info.linkUrl}`); },
-            args: [info]
+            func: (info) => {
+                alert(`The actual link is: ${info.linkUrl}`);
+            },
+            args: [info],
         });
     } else if (info.menuItemId === 'report') {
-        chrome.scripting.executeScript({
+        await chrome.scripting.executeScript({
             target: { tabId: (await getCurrentTab()).id },
-            func: (info) => { alert(`Reported link successfully`); },
-            args: [info]
+            func: (info) => {
+                alert(`Reported link successfully`);
+            },
+            args: [info],
         });
     }
 });
 
 chrome.action.onClicked.addListener(() => {
-    chrome.tabs.create({url: 'pages/setting/index.html'});
+    chrome.runtime.openOptionsPage();
 });
